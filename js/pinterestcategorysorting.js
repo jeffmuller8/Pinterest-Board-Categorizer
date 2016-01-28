aadb4ee465f08aaf44abc148bb934f12563160d2
jeffmuller8 <jeff.muller@cantina.co>
2016-01-28 16:40:45 -0500

    it works

--- js/pinterestcategorysorting.js
+++ js/pinterestcategorysorting.js
@@ -2,6 +2,9 @@ function init() {
 
  var tabBlock = document.getElementById("tabBlock");
 
+ var title,
+     openClose;
+
  for (var j = 0; j < board.board.length; j++) {
    var categoryTab = document.createElement("li");
    var target = document.createElement("a");
@@ -14,33 +17,63 @@ function init() {
    document.body.appendChild(boardHTML);
    boardHTML.className = "board";
    boardHTML.id = board.board[j].category;
-   var title = document.createElement("h1");
+   title = document.createElement("div");
    title.className = "title";
+   title.innerHTML = board.board[j].category.replace("_", " ");
+   openClose = document.createElement("div");
+   openClose.className = "openClose";
+   title.appendChild(openClose);
    boardHTML.appendChild(title);
+   openClose.innerHTML = "-";
    var pins = document.createElement("div");
    pins.className = "pins";
    boardHTML.appendChild(pins);
-   title.innerHTML = board.board[j].category.replace("_", " ");
-  }
+
+   openClose.addEventListener("click", function(e) {
+     var elem = e.target.parentNode.parentNode.querySelector(".pins");
+     toggleClass(elem, "hidden");
+     if (hasClass(elem, "hidden")) {
+       e.target.innerHTML = "+";
+     } else {
+       e.target.innerHTML = "-";
+       openClose.setAttribute("data-content", "-");
+     }
+   });
+ }
+
+ var test = document.querySelectorAll(".board");
+ test = Array.prototype.slice.call(test);
+
+   window.addEventListener("scroll", function(el) {
+     test.forEach( function(elem) {
+     if (isScrolledIntoView(elem)) {
+         console.log(elem.getAttribute('id'));
+        var id = elem.getAttribute('id');
+        //var onBoard = document.getElementById(id);
+        console.log(id);
+        console.log('.tab [href="#'+ id.replace(" ", "_") +'"]');
+        if (document.querySelector('.tab .active')) {
+          document.querySelector('.tab .active').className = "";
+        }
+
+        document.querySelector('.tab [href="#'+ id.replace(" ", "_") +'"]').className = "active";
+
+     }
+   });
+   });
+
+
+ function isScrolledIntoView(el) {
+    var elemTop = el.getBoundingClientRect().top + window.scrollY;
+    var elemBottom = el.getBoundingClientRect().bottom + window.scrollY;
+    var center = window.scrollY + window.innerHeight / 2;
+    return (center > elemTop && center < elemBottom);
+}
 
   var tab = document.querySelectorAll(".tab");
   var length = tab.length;
   tab[0].className = "tab top";
   tab[length-1].className = "tab bottom";
-
-  for (var i = 0; i < tab.length; i++) {
-
-   tab[i].addEventListener("click", function() {
-     for (var x = 0; x < tab.length; x++) {
-       tab[x].className = "tab";
-       tab[0].className += " top";
-       tab[tab.length-1].className += " bottom";
-     }
-       this.className = "tab--after";
-       tab[0].className += " top";
-       tab[tab.length-1].className += " bottom";
-   });
-  }
 
   $("#tabBlock .tab a").click(function(e) {
     e.preventDefault();
@@ -50,39 +83,66 @@ function init() {
     }, 500, "easeOutBack");
   });
 
-      function displayBoard() {
-        var boards = [];
-        for (var z = 0; z < board.board.length; z++) {
-          for (var c = 0; c < board.board[z].url.length; c++) {
-            boards.push(board.board[z].url[c]);
-        }
-        }
-        console.log(boards);
-        // var boards = [ 'zackargyle/burgers','zackargyle/travel-ideas'];
-        var BOARD_URL = "https://api.pinterest.com/v1/boards/{{board}}/pins/?access_token=AbIRvv67woWRh2yvuagaQzKMew58FClqyLz187BCtx3yBWBC3AAAAAA&fields=id%2Clink%2Cnote%2Curl%2Ccreated_at%2Cimage";
-        var promises = $.map(boards, function(boardOBJ) {
-        var boardOBJ = BOARD_URL.replace('{{board}}', boardOBJ);
-        return $.get(boardOBJ).then(function(response) {
-          return response.data;
-         });
-      });
-
-      $.when.apply($, promises).done(function() {
-        var boards = Array.prototype.slice.call(arguments);
-        $.each(boards, function(i, pins) {
-          $.each(pins, function(i, pin) {
-            var pinHTML = "<a data-pin-do='embedPin' href='"+pin.url+"'></a>";
-            var pints = document.getElementsByClassName("pins");
-
-                pints[1].innerHTML += pinHTML;
-
-
-
-          })
-        });
-        PinUtils.build();
-      });
+  function displayBoard() {
+    for (var i = 0; i < board.board.length; i++) {
+      for (var x = 0; x < board.board[i].url.length; x++) {
+        var pins = document.querySelector("#" + board.board[i].category + " .pins");
+         var pinHTML = '<a data-pin-do="embedBoard" data-pin-board-width="905" data-pin-scale-height="900" data-pin-scale-width="300" href="https://www.pinterest.com/'+board.board[i].url[x]+'"></a>';
+         pins.innerHTML += pinHTML;
       }
+    }
+    PinUtils.build();
+  }
+
+  function hasClass(elem, className) {
+
+      var classNames = elem.className.split(' ');
+
+      return classNames.indexOf(className) !== -1;
+
+  }
+
+  function addClass(elem, className) {
+
+      var classNames = elem.className.split(' ');
+
+      if (!hasClass(elem, className)) {
+
+          classNames.push(className);
+
+      }
+
+      elem.className = classNames.join(' ');
+
+  }
+
+  function removeClass(elem, className) {
+
+      var classNames = elem.className.split(' ');
+
+      if (hasClass(elem, className)) {
+
+          classNames.splice(classNames.indexOf(className), 1);
+
+      }
+
+      elem.className = classNames.join(' ');
+
+  }
+
+  function toggleClass(elem, className) {
+
+      if (!hasClass(elem, className)) {
+
+          addClass(elem, className);
+
+      } else {
+
+          removeClass(elem, className);
+      }
+
+  }
+
 displayBoard();
 };
 window.onload = init();
